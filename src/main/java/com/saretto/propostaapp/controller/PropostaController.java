e5f912b6a5dc5ccf7796742da12764d3336e0502
GabrielSaretto <gabriel.saretto2@gmail.com>
2024-07-30 22:49:10 -0300

    Criando o service

--- src/main/java/com/saretto/propostaapp/controller/PropostaController.java
+++ src/main/java/com/saretto/propostaapp/controller/PropostaController.java
@@ -2,6 +2,7 @@ package com.saretto.propostaapp.controller;
 
 import com.saretto.propostaapp.dto.PropostaRequestDto;
 import com.saretto.propostaapp.dto.PropostaResponseDto;
+import com.saretto.propostaapp.service.PropostaService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/proposta")
 public class PropostaController {
 
+    private PropostaService propostaService;
+
     @PostMapping
     public ResponseEntity<PropostaResponseDto> criar(@RequestBody PropostaRequestDto requestDto) {
-        return null;
+        PropostaResponseDto response = propostaService.criar(requestDto);
+        return ResponseEntity.ok(response);
     }
 }
---
+++ src/main/java/com/saretto/propostaapp/service/PropostaService.java
@@ -0,0 +1,11 @@
+package com.saretto.propostaapp.service;
+
+import com.saretto.propostaapp.dto.PropostaRequestDto;
+import com.saretto.propostaapp.dto.PropostaResponseDto;
+
+public class PropostaService {
+
+    public PropostaResponseDto criar(PropostaRequestDto requestDto){
+        return null;
+    }
+}
