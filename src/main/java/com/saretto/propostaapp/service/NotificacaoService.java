7a6d379d8af4aef00bf5871e77ea80321977f6fa
GabrielSaretto <gabriel.saretto2@gmail.com>
2024-08-18 08:38:22 -0300

    Implementando resiliência na aplicação

--- src/main/java/com/saretto/propostaapp/service/NotificacaoService.java
+++ src/main/java/com/saretto/propostaapp/service/NotificacaoService.java
@@ -1,6 +1,6 @@
 package com.saretto.propostaapp.service;
 
-import com.saretto.propostaapp.dto.PropostaResponseDto;
+import com.saretto.propostaapp.entity.Proposta;
 import lombok.AllArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
@@ -11,7 +11,7 @@ public class NotificacaoService {
 
     private RabbitTemplate rabbitTemplate;
 
-    public void notificar(PropostaResponseDto proposta, String exchange){
+    public void notificar(Proposta proposta, String exchange){
         rabbitTemplate.convertAndSend(exchange, "", proposta);
     }
 }
